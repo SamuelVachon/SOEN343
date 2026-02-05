478883cc5c5a00bd9f0a2eea92d765902d6b2309
SamuelVachon <SamuelVachon2003@hotmail.com>
2026-02-05 16:29:03 -0500

    Added .gitignore

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,30 +1,23 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+require('dotenv').config();
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBCEd2DrUKDXJX2oOWsahKiZMzaJa29JoA",
-  authDomain: "soen-343-risky-bandits.firebaseapp.com",
-  projectId: "soen-343-risky-bandits",
-  storageBucket: "soen-343-risky-bandits.firebasestorage.app",
-  messagingSenderId: "848682659503",
-  appId: "1:848682659503:web:b364d5aa3fdfb8820cee5a",
-  measurementId: "G-FYM27YTM1Y"
-};
 
-// const firebaseConfig = {
-//   apiKey: "YOUR_API_KEY",
-//   authDomain: "YOUR_AUTH_DOMAIN",
-//   projectId: "YOUR_PROJECT_ID",
-//   storageBucket: "YOUR_STORAGE_BUCKET",
-//   messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-//   appId: "YOUR_APP_ID",
-// };
+
+const firebaseConfig = {
+   apiKey: process.env.YOUR_API_KEY,
+   authDomain: process.env.YOUR_AUTH_DOMAIN,
+   projectId: process.env.YOUR_PROJECT_ID,
+   storageBucket: process.env.YOUR_STORAGE_BUCKET,
+   messagingSenderId: process.env.YOUR_MESSAGING_SENDER_ID,
+   appId: process.env.YOUR_APP_ID,
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
